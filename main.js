68c34a528a759e5c98b341032dd38e5c8da681b1
carlosmellocarvalho <carlos.hmc@gmail.com>
2023-07-11 17:45:37 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('form-contato');
+const contatos = [];
+const telefones = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputTelefoneContato = document.getElementById('telefone-contato');
+
+    if (contatos.includes(inputNomeContato.value)) {
+        alert(`A atividade: ${inputNomeContato.value} já foi inserida`)
+    } else {
+        contatos.push(inputNomeContato.value);
+        telefones.push(parseFloat(inputTelefoneContato.value));
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputTelefoneContato.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+
+    inputNomeContato.value = '';
+    inputTelefoneContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
